d604bc92225d331f77abdc56a0f18ed7b10d7b10
kekalrezeki <mochamadrizki124@gmail.com>
2025-11-03 16:30:33 +0700

    Add files via upload

---
+++ PRAKTIKUMMMMM1.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praktikummmmm1;
+
+/**
+ *
+ * @author achma
+ */
+public class PRAKTIKUMMMMM1 {
+
+     public static void main(String[] args) {
+        String nama,classRPL;
+        int angka1,angka2;
+        double desbil1,desbil2;
+        
+        nama = "Alex";
+                classRPL = "RPL2" ;
+                angka1 = 20;
+                angka2 = 90;
+                desbil1 = 12.3;
+                desbil2 = 15.77;
+                
+                System.out.println("Nama Saya : "+nama);
+                 System.out.println("Saya Kelas : "+classRPL);
+                 angka1+= angka2;
+                 System.out.println("Hasil angka1 = : "+angka1);
+                 desbil1=desbil2;
+                 System.out.println("Hasil desbil1 : "+desbil1);
+                angka1*=desbil2;
+                System.out.println("Hasil angka1 : "+angka1);
+                angka2%=desbil1;
+                System.out.println("Hasil angka2 : "+angka2);
+               
+    }
+}
